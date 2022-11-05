052f2759335cc84f63e4442fc879cd207890fe3d
Dope Undawater <worksingletonmcdaniel@gmail.com>
2022-11-04 22:40:57 -0700

    psuedo code and js files added

---
+++ index.js
@@ -0,0 +1,33 @@
+// View All departments
+// SELECT * FROM departments;
+
+// View all roles
+// SELECT * FROM roles;
+
+// view all employees
+// SELECT * FROM employees;
+
+
+// Create new departments 
+
+// prompt the user for the "name" of the department
+
+        // Then Run the Query
+        // INSERT INTO departments(name)
+        // VALUES ('sales');
+
+              // THEN ask the user what they want to do next?
+
+
+// Create a new role 
+
+// Get The Existing Deparments from the 'department' table
+
+        // THEN prompt the user for the "title", "salary" , and "department  for the role"
+
+        // THEN Run the Query 
+        // INSERT INTO role (title, sale, department_id)
+        // VALUES ("Manager", 120000, 1)
+
+            
+            // THEN ask the user what they want to do next?
---
+++ server.js
@@ -0,0 +1,22 @@
+// require express
+const express = require("express")
+
+const inquirer = require("inquirer")
+// require `db.json` file and store it in `notes`
+
+// use express to initialize the `app` server `const app = express;`
+
+
+// Get /notes should return the notes.html file.
+
+// GET * should return the `index.html` file.
+
+// use the app to listen to a specific port.
+
+
+
+// 
+
+
+
+// JSON.stringify our updated info.
