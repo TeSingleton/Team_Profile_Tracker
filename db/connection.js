ab025b723a1a797cd41932e24676ecaafa98a0f1
Te <worksingletonmcdaniel@gmail.com>
2022-11-05 19:13:04 -0700

    adding js and schema

---
+++ db/connection.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+const connection = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username below if different
+      user: 'root',
+      // Your MySQL password below if used, if not leave as is
+      password: '',
+      database: 'employees_db'
+    },
+    // console.log(`Connected to the employees_db database.`)
+  );
+
+  connection.connect(function (err) {
+      if (err) {
+        throw err;
+      } else {
+          // console.log('Sucessfully connected to mysql!')
+      }
+  });
