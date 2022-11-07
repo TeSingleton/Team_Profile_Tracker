7c7dedb1fb629f9bcda8a73bec8f9c76d623dac0
Te <worksingletonmcdaniel@gmail.com>
2022-11-06 17:10:48 -0800

    changes made to correct schema

--- db/connection.js
+++ db/connection.js
@@ -1,20 +1,20 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 const connection = mysql.createConnection(
-    {
-      host: '127.0.0.1',
-      // Your MySQL username below if different
-      user: 'root',
-      // Your MySQL password below if used, if not leave as is
-      password: '',
-      database: 'employees_db'
-    },
-    // console.log(`Connected to the employees_db database.`)
-  );
+  {
+    host: "localhost",
+    // Your MySQL username below if different
+    user: "root",
+    // Your MySQL password below if used, if not leave as is
+    password: "054332",
+    database: "employees_db",
+  }
+  // console.log(`Connected to the employees_db database.`)
+);
 
-  connection.connect(function (err) {
-      if (err) {
-        throw err;
-      } else {
-          // console.log('Sucessfully connected to mysql!')
-      }
-  });
+connection.connect(function (err) {
+  if (err) {
+    throw err;
+  } else {
+    // console.log('Sucessfully connected to mysql!')
+  }
+});
--- db/schema.sql
+++ db/schema.sql
@@ -4,28 +4,34 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE department(
-    id INT AUTO_INCREMENT NOT NULL,
-    dep_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id)
-);
+CREATE TABLE
+    departments(
+        id INT AUTO_INCREMENT NOT NULL,
+        dep_name VARCHAR(30) NOT NULL,
+        PRIMARY KEY(id)
+    );
 
-CREATE TABLE emp_role(
-        id INT AUTO_INCREMENT ,
+CREATE TABLE
+    empRole(
+        id INT AUTO_INCREMENT,
         title VARCHAR(30),
-        salary DEC(15,2),
+        salary DEC(15, 2),
+        department_id INT,
         PRIMARY KEY(id),
-        Foreign Key (department_id) REFERENCES department (id) ON DELETE SET NULL
-        
-);
+        Foreign Key (department_id) REFERENCES departments (id) ON DELETE
+        SET NULL
+    );
 
-CREATE TABLE employees(
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    emp_role_id INT,
-    manager_id INT,
-    Foreign Key (emp_role_id) REFERENCES emp_role (id) ON DELETE SET NULL,
-    Foreign Key (manager_id) REFERENCES employees(id) ON DELETE SET NULL
-
-);
+CREATE TABLE
+    employees(
+        id INT PRIMARY KEY,
+        first_name VARCHAR(30),
+        last_name VARCHAR(30),
+        empRole_id INT,
+        manager_id INT,
+        Foreign Key (empRole_id) REFERENCES empRole (id) ON DELETE
+        SET
+            NULL,
+            Foreign Key (manager_id) REFERENCES employees(id) ON DELETE
+        SET NULL
+    );
